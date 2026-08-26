283e23304b82af150c4d6129494a850a7cdac313
Alini Silva <alini.silva2005@gmail.com>
2026-08-25 23:51:49 -0300

    Add main - Versionamento electron

---
+++ main.js
@@ -0,0 +1,38 @@
+//const AppDatabase = require('./src/db/database');
+
+const { app, BrowserWindow } = require('electron/main');
+const path = require('node:path');
+
+let db;
+
+
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('front-end/index.html')
+}
+
+app.whenReady().then(() => {
+    //db = new AppDatabase();
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+//Quando todas as janelas estão fechadas o app fecha
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
