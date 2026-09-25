61dc63b70802a1452af0e76dcabd5e7a7bd557ac
Alini Silva <alini.silva2005@gmail.com>
2026-09-25 12:56:29 -0300

    Refresh dos paineis após alguma função (cadastro ou emprestimo)

--- front-end/script.js
+++ front-end/script.js
@@ -1261,7 +1261,8 @@ function setupReturnsList() {
           
           if (resultado.ok) {
             // Recarrega a tabela para o livro sumir da tela
-            renderReturns(); 
+            await loadActiveLoans();
+            await loadCollectionBooks();  
           } else {
             alert("Erro ao devolver: " + resultado.message);
           }
@@ -1396,6 +1397,7 @@ async function handleLoanSubmit(event) {
       loanFormMessage.textContent = resultado.message;
       loanFormMessage.style.color = "green";
       await loadActiveLoans();//atualiza lista de devoluções apos o emprestimo
+      await loadCollectionBooks(); //puxa a função responsavel por buscar as novas infos do acervo
       loanForm.reset(); 
       bookInput.setAttribute('data-book-id', ''); // Limpa o ID escondido
       loanForm.querySelector('[name="studentName"]').focus(); 
